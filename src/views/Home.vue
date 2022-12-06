e4c343c7d2c51274877ead1046131d1d46c2855b
wooyeol <dnduf158@gmail.com>
2022-12-07 02:16:16 +0900

    feat : home/login 페이지 [이우열]

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,143 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="lol">
+      <div class="circle circle_1"></div>
+      <div class="circle circle_2"></div>
+      <div class="circle circle_3"></div>
+      <div class="circle circle_4"></div>
+      <div class="circle circle_5"></div>
+      <div class="circle circle_6"></div>
+      <div class="circle circle_7"></div>
+      <div class="circle circle_8"></div>
+      <div class="circle circle_9"></div>
+      <div class="circle circle_10"></div>
+      <div class="circle circle_11"></div>
+      <div class="circle circle_12"></div>
+      <div class="circle circle_13"></div>
+      <div class="circle circle_14"></div>
+      <div class="circle circle_15"></div>
+      <div class="circle circle_16"></div>
+    </div>
+    <!-- <img class="logo" alt="Vue logo" src="../assets/gasliMBTIng-logo.png"> -->
+    <h1 class="title__custom">gasli<span class="title__m">M</span><span class="title__b">B</span><span class="title__t">T</span><span class="title__i">I</span>ng</h1>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
 }
 </script>
+
+<style lang="scss">
+.home {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.lol {
+  position: relative;
+  margin-top: 80px;
+  width: 300px;
+  height: 300px;
+  border-radius: 50%;
+  background-image: url("../assets/gaslight.png");
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: contain;
+}
+.circle {
+  position: absolute;
+  width: 25px;
+  height: 25px;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: contain;
+  border-radius: 50%;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  overflow: hidden;
+  margin: auto;
+}
+
+@for $i from 1 to 5 {
+  .circle_#{$i} {
+    background-image: url("../assets/flower_red.png");
+    -webkit-animation: circle_#{$i} 10s linear infinite;
+    animation: circle_#{$i} 10s linear infinite;
+  }
+}
+
+@for $i from 5 to 9 {
+  .circle_#{$i} {
+    background-image: url("../assets/flower_green.png");
+    -webkit-animation: circle_#{$i} 10s linear infinite;
+    animation: circle_#{$i} 10s linear infinite;
+  }
+}
+
+@for $i from 9 to 13 {
+  .circle_#{$i} {
+    background-image: url("../assets/flower_blue.png");
+    -webkit-animation: circle_#{$i} 10s linear infinite;
+    animation: circle_#{$i} 10s linear infinite;
+  }
+}
+
+@for $i from 13 to 17 {
+  .circle_#{$i} {
+    background-image: url("../assets/flower_yellow.png");
+    -webkit-animation: circle_#{$i} 10s linear infinite;
+    animation: circle_#{$i} 10s linear infinite;
+  }
+}
+
+@for $i from 1 to 17 {
+  .circle_#{$i} {
+    @-webkit-keyframes circle_#{$i} {
+      0% {
+        transform: rotate(($i - 1) * 22.5deg)translate(-150px);
+      }
+      100% {
+        transform: rotate(360 + ($i - 1) * 22.5deg)translate(-150px);
+      }
+    }
+    
+    @keyframes circle_#{$i} {
+      0% {
+        transform: rotate(($i - 1) * 22.5deg)translate(-150px);
+      }
+      100% {
+        transform: rotate(360 + ($i - 1) * 22.5deg)translate(-150px);
+      }
+    }
+  }
+}
+
+.logo {
+  width: 300px;
+  background: #EAE4E9;
+  border-radius: 20px;
+}
+.title__custom {
+  margin-top: 50px;
+  font-size: 80px;
+  text-decoration: underline;
+}
+.title__m {
+  color: #FF99C8;
+}
+.title__b {
+  color: #fff171;
+}
+.title__t {
+  color: #a2f3c1;
+}
+.title__i {
+  color: #88d2f7;
+}
+</style>
---
+++ src/views/LogIn.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="container">
+    <div class="columns is-centered">
+      <div class="column is-10">
+        <h1 class="title">Log in</h1>
+
+        <form @submit.prevent="submitForm">
+          <div class="field">
+            <label>Username</label>
+            <div class="control">
+              <input type="text" name="username" class="input" v-model="username">
+            </div>
+          </div>
+
+          <div class="field">
+            <label>Password</label>
+            <div class="control">
+              <input type="password" name="password" class="input" v-model="password">
+            </div>
+          </div>
+
+          <div class="notification is-danger" v-if="errors.length">
+            <p v-for="error in errors" v-bind:key="error">{{ error }}</p>
+          </div>
+
+          <div class="field">
+            <div class="control">
+              <button class="button is-success">Submit</button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import axios from 'axios'
+
+  export default {
+    name: 'LogIn',
+    data() {
+      return {
+        username: '',
+        password: '',
+        errors: [],
+      }
+    },
+    methods: {
+      async submitForm() {
+        this.$store.commit('setIsLoading', true)
+
+        axios.defaults.headers.common['Authorization'] = ''
+        localStorage.removeItem('token')
+
+        const formData = {
+          username: this.username,
+          password: this.password,
+        }
+
+        await axios
+          .post('/api/v1/token/login/', formData)
+          .then(response => {
+            const token = response.data.auth_token
+
+            this.$store.commit('setToken', token)
+
+            axios.defaults.headers.common['Authorization'] = 'Token ' + token
+
+            localStorage.setItem('token', token)
+          })
+          .catch(error => {
+            if (error.response) {
+              for (const property in error.response.data) {
+                this.errors.push(`${property}: ${error.response.data[property]}`)
+              }
+            } else if (error.message) {
+              this.errors.push('Something went wrong. Please try again!')
+            }
+          })
+
+        await axios
+          .get('/api/v1/users/me')
+          .then(response => {
+            this.$store.commit('setUser', {'id': response.data.id, 'username': response.data.username})
+
+            localStorage.setItem('username', response.data.username)
+            localStorage.setItem('userid', response.data.id)
+          })
+          .catch(error => {
+            console.log(error)
+          })
+        
+        this.$store.commit('setIsLoading', false)
+      }
+    }
+  }
+</script>
