bb6e3cb0883baaa7b48b5ed2be044206100f6efd
wxxk <dwde22@gmail.com>
2022-12-08 17:45:56 +0900

    feat : mbti / detail [이상욱]

--- src/views/dashboard/Mbti.vue
+++ src/views/dashboard/Mbti.vue
@@ -1,19 +1,31 @@
 <template>
   <div class="container">
-    <div class="columns is-multiline">
-      <div class="column is-12">
+    <div class="columns is-multiline is-centered">
+      <div class="column is-10">
         <h1 class="title">MBTI</h1>
+
+        <!-- <div class="field">
+          <div class="control">
+            <div class="select">
+              <select v-model="mbtis">
+                <option value="MBTI">MBTI</option>
+                <option v-for="mbti_ in mbti" v-bind:key="mbti_id" value="{{mbti_.mbti}}">{{mbti_.mbti}}</option>
+              </select>
+            </div>
+          </div>
+        </div> -->
 
         <router-link to="/dashboard/mbti/add">Add MBTI</router-link>
       </div>
 
-      <div class="column is-12">
+      <div class="column is-10">
         <table class="table is-fullwidth">
           <thead>
             <tr>
               <th>MBTI</th>
               <th>title</th>
               <th>content</th>
+              <th></th>
             </tr>
           </thead>
 
@@ -22,8 +34,12 @@
               <td>{{ mbti_.mbti }}</td>
               <td>{{ mbti_.title }}</td>
               <td>{{ mbti_.content }}</td>
+              <td>
+                <router-link :to="{ name: 'MbtiDetail', params: { id: mbti_.id }}">Details</router-link>
+              </td>
             </tr>
           </tbody>
+
         </table>
       </div>
     </div>
@@ -49,7 +65,7 @@
         this.$store.commit('setIsLoading', true)
 
         axios
-          .get('MBTI/')
+          .get('/MBTI/')
           .then(response => {
             this.mbti = response.data
           })
