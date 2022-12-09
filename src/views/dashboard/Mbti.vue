23b3bca4130be5a68441b8310fefbf01551a58cc
wxxk <dwde22@gmail.com>
2022-12-09 11:42:29 +0900

    update : mbti 게시판 분류 [이상욱]

--- src/views/dashboard/Mbti.vue
+++ src/views/dashboard/Mbti.vue
@@ -4,18 +4,48 @@
       <div class="column is-10">
         <h1 class="title">MBTI</h1>
 
-        <!-- <div class="field">
+        <div class="field">
           <div class="control">
-            <div class="select">
-              <select v-model="mbtis">
-                <option value="MBTI">MBTI</option>
-                <option v-for="mbti_ in mbti" v-bind:key="mbti_id" value="{{mbti_.mbti}}">{{mbti_.mbti}}</option>
-              </select>
-            </div>
-          </div>
-        </div> -->
+            
+            <div class="radio-tile-group">
+              
+              <div class="input-container">
+                <input type="radio" id="dot-1" v-model="filters" value="전체">
+                <div class="radio-tile">
+                  <label for="dot-1">전체</label>
+                </div>
+              </div>
+              
+              <div class="input-container">
+                <input type="radio" id="dot-2" v-model="filters" value="상대법">
+                <div class="radio-tile">
+                  <label for="dot-2">상대법</label>
+                </div>
+              </div>
+              
+              <div class="input-container">
+                <input type="radio" id="dot-3" v-model="filters" value="주의할 점">
+                <div class="radio-tile">
+                  <label for="dot-3">주의할 점</label>
+                </div>
+              </div>
+              
+              <div class="input-container">
+                <input type="radio" id="dot-4" v-model="filters" value="특징">
+                <div class="radio-tile">
+                  <label for="dot-4">특징</label>
+                </div>
+              </div>
 
-        <router-link to="/dashboard/mbti/add">Add MBTI</router-link>
+            </div>
+
+
+          </div>
+        </div>
+        
+        <hr>
+
+        <router-link to="/dashboard/mbti/add" class="button is-light">글쓰기</router-link>
       </div>
 
       <div class="column is-10">
@@ -29,12 +59,34 @@
             </tr>
           </thead>
 
-          <tbody>
+          <!-- <tbody>
             <tr v-for="mbti_ in mbti" v-bind:key="mbti_.id">
               <td>{{ mbti_.mbti }}</td>
               <td>{{ mbti_.title }}</td>
               <td>{{ mbti_.content }}</td>
               <td>
+                <router-link :to="{ name: 'MbtiDetail', params: { id: mbti_.id }}">Details</router-link>
+              </td>
+            </tr>
+          </tbody> -->
+
+          <tbody v-if="filters == '전체'">
+            <tr v-for="mbti_ in mbti" v-bind:key="mbti_.id">
+              <td>{{ mbti_.mbti }}</td>
+              <td>{{ mbti_.title }}</td>
+              <td>{{ mbti_.content }}</td>
+              <td>
+                <router-link :to="{ name: 'MbtiDetail', params: { id: mbti_.id }}">Details</router-link>
+              </td>
+            </tr>
+          </tbody>
+
+          <tbody v-if="filters != '전체'">
+            <tr v-for="mbti_ in mbti" v-bind:key="mbti_.id">
+              <td v-if="(mbti_.board == filters)">{{ mbti_.title }}</td>
+              <td v-if="(mbti_.board == filters)">{{ mbti_.mbti }}</td>
+              <td v-if="(mbti_.board == filters)">{{ mbti_.content }}</td>
+              <td v-if="(mbti_.board == filters)">
                 <router-link :to="{ name: 'MbtiDetail', params: { id: mbti_.id }}">Details</router-link>
               </td>
             </tr>
@@ -54,7 +106,8 @@
     name: 'Mbti',
     data() {
       return {
-        mbti: []
+        mbti: [],
+        filters: '전체'
       }
     },
     created() {
@@ -78,3 +131,46 @@
     }
   }
 </script>
+
+<style>
+  hr {
+    background-color: #EAE4E9;
+  }
+
+  .radio-tile-group {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+
+  .input-container {
+    position: relative;
+    height: 40px;
+    width: 80px;
+    margin: 0.5rem;
+  }
+
+  .input-container input{
+    position: absolute;
+    height: 100%;
+    width: 100%;
+    margin: 0;
+    cursor: pointer;
+    z-index: 2;
+    opacity: 0;
+  }
+
+  .input-container .radio-tile{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 40px;
+    border: 2px solid #EAE4E9;
+    border-radius: 10px;
+  }
+
+  input:checked + .radio-tile {
+    background-color: #EAE4E9;
+  }
+</style>
