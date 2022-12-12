906520929c1beade776acdb70e9b3fda318262a4
wooyeol <dnduf158@gmail.com>
2022-12-12 19:51:42 +0900

    feat : main/아이콘 추가 [이우열]

--- src/main.js
+++ src/main.js
@@ -4,10 +4,11 @@ import router from './router'
 import store from './store'
 import axios from 'axios'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faUserSecret, faHouse, faCircleUser, faBook, faComments, faRightToBracket, faAddressBook, faEnvelope, faLock, faFaceSmile, faBaby, faMars, faVenus } from '@fortawesome/free-solid-svg-icons'
+import { faUserSecret, faHouse, faCircleUser, faBook, faComments, faRightToBracket, faAddressBook, faEnvelope, faLock, faFaceSmile, faBaby, faMars, faVenus, faBell, faArrowLeft } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-library.add(faUserSecret, faHouse, faCircleUser, faBook, faComments, faRightToBracket, faAddressBook, faEnvelope, faLock, faFaceSmile, faBaby, faMars, faVenus);
+library.add(faUserSecret, faHouse, faCircleUser, faBook, faComments, faRightToBracket, faAddressBook, faEnvelope, faLock, faFaceSmile, faBaby, faMars, faVenus, faBell, faArrowLeft);
+import vueMoment from 'vue-moment'
 
 axios.defaults.baseURL = process.env.VUE_APP_API_URL
 
-createApp(App).use(store).use(router, axios).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
+createApp(App).use(store).use(router, axios, vueMoment).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
