fbbf9e5bdd21531509ba20f303b7d1dcdeecc1bf
rhoeunbin <rhoeb21@gmail.com>
2022-12-13 16:29:35 +0900

    chore : guestbook 수정 [노은빈]

--- src/views/dashboard/Profile.vue
+++ src/views/dashboard/Profile.vue
@@ -2,63 +2,64 @@
   <div class="container">
     <div class="columns is-multiline" style= "justify-content: center;">
       <div class="column is-10 is-center">
-        <div class="box">
-          <h1 class="title">{{ user.username }}님의 회원정보</h1>
-          <div style="display: flex; align-items: center; justify-content: space-around;">
-            <img :src="user.image" class="img_profile">
-            <span>
-              <p class="mbti_font"><strong>이메일: </strong> {{ user.username }} </p>
-              <p class="mbti_font"><strong>닉넴: </strong> {{ user.nickname }} </p>
-              <p class="mbti_font"><strong>MBTI: </strong>{{ user.mbti1 }}{{ user.mbti2 }}{{ user.mbti3 }}{{ user.mbti4 }} </p>
-              <p class="mbti_font"><strong>나이: </strong> {{ user.age }} </p> 
-              <p class="mbti_font"><strong>성별: </strong> {{ user.gender }} </p>
-            </span>
-          </div>
 
-          <div class="column is-10" style="text-align: center; margin: auto;">
-            <div class="buttons" style=" display: inline-block;">
-              <router-link :to="{ name: 'EditAccount' }" class="button is-success">프로필 편집</router-link>
-              <button @click="logout()" class="button is-danger">로그아웃</button>
-            </div>
+        <!-- 회원 정보 -->
+        <div class="box">
+          <h1 class="title">{{ user.nickname }} 님의 회원정보</h1>
+          <div class="profile__info">
+            <img :src="user.image" class="img__profile">
+            <span>
+              <p class="mbti__font"><strong>이메일: </strong> {{ user.username }} </p>
+              <p class="mbti__font"><strong>MBTI: </strong>{{ user.mbti1 }}{{ user.mbti2 }}{{ user.mbti3 }}{{ user.mbti4 }} </p>
+              <p class="mbti__font"><strong>나이: </strong> {{ user.age }} </p> 
+              <p class="mbti__font"><strong>성별: </strong> {{ user.gender }} </p>
+            </span>
           </div>
         </div>
       </div> 
 
+      <!-- 방명록 -->
       <div class="column is-10 is-center">
         <div class="box">
           <h1 class="title">방명록</h1>
-          <router-link :to="{ name: 'AddGuestbook', params: { id: Number(user.id) }}">글쓰기</router-link>
-          <div v-for="guest in guestlist" v-bind:key="guest.id">
-            {{ guest.name }}, {{ guest.contents }}
+          <div class="write__div">
+            <router-link :to="{ name: 'AddGuestbook', params: { id: Number(user.id) }}" class="write__btn is-light">글쓰기</router-link>
           </div>
+          <div v-for="guest in guestlist" v-bind:key="guest.id">
+              <router-link class="guest__link" :to="{ name: 'Guestbook', params: { id: guest.id }}">
+              <p class="guest__content">{{ guest.name }} | {{ guest.contents }}</p>
+              </router-link>
+            </div>
         </div>
       </div> 
+
+      <!-- 친구 분포도 -->
       <div class="column is-10 is-center">
         <div class="box">
           <h1 class="title">친구 분포도</h1>
           <div>
-            <div class="mbti_friend">
-              <div><span class="mbti_left">E</span> <span class="mbti_right">I</span></div>
-              <div class="bar-container">
-                <div id="content_bar" class="bar1"></div>
+            <div class="mbti__friend">
+              <div><span class="mbti__left">E</span> <span class="mbti__right">I</span></div>
+              <div class="bar__container">
+                <div id="content__bar" class="bar1"></div>
               </div> 
             </div>           
-            <div class="mbti_friend">
-              <div><span class="mbti_left">N</span> <span class="mbti_right">S</span></div>
-              <div class="bar-container">
-                <div id="content_bar" class="bar2"></div>
+            <div class="mbti__friend">
+              <div><span class="mbti__left">N</span> <span class="mbti__right">S</span></div>
+              <div class="bar__container">
+                <div id="content__bar" class="bar2"></div>
               </div>           
             </div>
-            <div class="mbti_friend">
-              <div><span class="mbti_left">T</span> <span class="mbti_right">F</span></div>
-                <div class="bar-container">
-                  <div id="content_bar" class="bar3"></div>
+            <div class="mbti__friend">
+              <div><span class="mbti__left">T</span> <span class="mbti__right">F</span></div>
+                <div class="bar__container">
+                  <div id="content__bar" class="bar3"></div>
                 </div>           
             </div>
-            <div class="mbti_friend">
-              <div><span class="mbti_left">P</span> <span class="mbti_right">J</span></div>
-                <div class="bar-container">
-                  <div id="content_bar" class="bar4"></div>
+            <div class="mbti__friend">
+              <div><span class="mbti__left">P</span> <span class="mbti__right">J</span></div>
+                <div class="bar__container">
+                  <div id="content__bar" class="bar4"></div>
                 </div>           
             </div>            
           </div>  
@@ -126,39 +127,57 @@
   }
 </script>
 
-<style>
-.img_profile {
+<style scoped>
+.write__div {
+  position: sticky;
+  top: 6rem;
+  display: flex;
+  justify-content: center;
+}
+.write__btn {
+  padding: 0.3rem 1.5rem;
+  background: #bd32fd;
+  color: #fff;
+  border-radius: 10rem;
+  border: 2px dashed rgb(255, 255, 255);
+  box-shadow: 0 0 0 0.2rem #bd32fd;
+}
+.write__btn:hover {
+  background: #9f22da;
+  color: #fff;
+  box-shadow: 0 0 0 0.2rem #9f22da;
+}
+.profile__info {
+  display: flex; 
+  align-items: center; 
+  justify-content: space-around;
+}
+.img__profile {
   width: 5rem;
   vertical-align: middle;
 }
-
-.mbti_font {
+.mbti__font {
   font-size: 20px;
 }
-
-.mbti_font1 {
+.mbti__font1 {
   font-size: 20px;
   font-weight: bold;
 }
-
-.mbti_friend {
+.mbti__friend {
   font-size: 20px;
   display: flex;
   flex-direction: column;
   
 }
-
-.mbti_left {
+.mbti__left {
   float: left;
   font-weight: bold;
 }
-
-.mbti_right {
+.mbti__right {
   float: right;
   font-weight: bold;
 }
-
-.bar-container {
+.bar__container {
   width: 100%;
   background-color: #f1f1f1;
   text-align: center;
@@ -166,34 +185,32 @@
   border-radius: 5px;
   margin: 0;
 }
-
 .bar1 {
   height: 20px;
   background-color: #FF99C8;
   border-radius: 5px;
   width:55%
 }
-
 .bar2 {
   height: 20px;
   background-color: #FCF6BD;
   border-radius: 5px;
   width:55%
 }
-
 .bar3 {
   height: 20px;
   background-color: #D0F4DE;
   border-radius: 5px;
   width:55%
 }
-
 .bar4 {
   height: 20px;
   background-color: #A9DEF9;
   border-radius: 5px;
   width:55%
 }
-
+.guest__content {
+  font-size: 1.3rem;
+}
 </style>
 
